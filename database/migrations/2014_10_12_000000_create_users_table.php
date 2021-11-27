30ded76f51f524965f85c3b64e53cd5e87f07b57
Muhamad Alfisyah Reza Daulay <daulayreza@gmail.com>
2021-11-27 08:52:18 +0700

    tambah nomor dan status user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->string('nomor_karyawan')->unique()->index()->nullable();
+            $table->string('status', 20)->index();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
